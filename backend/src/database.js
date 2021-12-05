17119ff4bfbd3bd7702b90295bf9eb6ee70f917d
Cristian Vitola <chriss.vian@gmail.com>
2021-12-05 01:18:43 -0500

    agregando mongo atlas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/HeladeriaLaEsperanza';
+//const url = 'mongodb://localhost:27017/HeladeriaLaEsperanza';
+const url = 'mongodb+srv://user_cristian:Rollshake2021@storagecv.kprqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const options = {useNewUrlParser: true, useUnifiedTopology: true};
 
